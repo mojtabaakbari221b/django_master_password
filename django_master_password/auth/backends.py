eb142192efae058921856634f389ac1f0d2ace64
mojtabaakbari221b <mojtaba.akbari.221b@gmail.com>
2022-06-18 16:46:43 +0430

    complete logic of master password

--- django_master_password/auth/backends.py
+++ django_master_password/auth/backends.py
@@ -1,4 +1,24 @@
 from django.contrib.auth.backends import BaseBackend
+from django.conf import settings
+from django.contrib.auth import get_user_model
+User = get_user_model()
 
 class MasterPasswordBackend(BaseBackend):
-    pass
+    def authenticate(self, request, username=None, password=None):
+        user = request.user
+        if user.is_authenticated and user.is_staff :
+            use_admin_username_from_settings = getattr( settings, 'USE_ADMIN_USERNAME_FROM_SETTINGS', False )
+            if use_admin_username_from_settings :
+                if username == getattr( settings, 'ADMIN_LOGIN', False ):
+                    user = self.get_user_with_given_username(admin_user=user, username=username)
+            else :
+                user = self.get_user_with_given_username(admin_user=user, username=username)
+
+            return user
+    
+    def get_user_with_given_username(self, admin_user, username):
+        try:
+            user = User.objects.get(username=username)
+        except User.DoesNotExist:
+            user = admin_user
+        return user
