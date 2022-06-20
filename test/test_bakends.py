75b7ac9d9ed036306135edbf3047c76ae80047e6
mojtabaakbari221b <mojtaba.akbari.221b@gmail.com>
2022-06-20 18:42:17 +0430

    test login without USE_ADMIN_USERNAME_FROM_SETTINGS if username dosent exist

--- test/test_bakends.py
+++ test/test_bakends.py
@@ -1,13 +1,30 @@
-from django.test import TestCase
+from django.test import TestCase, override_settings
+from django.contrib.auth import authenticate
+from django.http import HttpRequest
 from django.contrib.auth import get_user_model
 User = get_user_model()
 
 class TestMasterPasswordBackend(TestCase):
+    def setUp(self):
+        self._password = 'admin'
+        self.user = User.objects.create_user(
+            username = 'admin',
+            email = 'admin@admin.com',
+            is_staff = True,
+            is_active = True,
+            is_superuser = True,
+            password = 'admin',
+        )
+
+        self.request = HttpRequest()
+        self.request.user = self.user
+
+    @override_settings(USE_ADMIN_USERNAME_FROM_SETTINGS=False)
     def test_valid_username_for_login_without_USE_ADMIN_USERNAME_FROM_SETTINGS(self):
         pass
 
     def test_invalid_username_for_login_without_USE_ADMIN_USERNAME_FROM_SETTINGS(self):
-        pass
+        self.assertEqual(authenticate(request=self.request, username='sample', password='sample'), self.user)
 
     def test_valid_admin_for_login_with_USE_ADMIN_USERNAME_FROM_SETTINGS(self):
         pass
